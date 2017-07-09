bb40256cfde58f7a64c071bef18c97d7f6cde083
Glowsquirrel <kalan.hunter@byu.net>
2017-07-09 01:25:19 -0600

    Reorganized file structure

--- Ticket_To_Ride/app/src/main/java/Interfaces/Observable.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-
-public interface Observable {
-    public void register(Observer o);
-    public void unregister(Observer o);
-    public void notifyObserver();
-}
--- Ticket_To_Ride/server/src/main/java/Interfaces/ICommand.java
+++
@@ -1,7 +0,0 @@
-package Interfaces;
-
-import CommandResults.CommandResult;
-
-public interface ICommand {
-    CommandResult execute();
-}
