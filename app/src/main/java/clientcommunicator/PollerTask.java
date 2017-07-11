a1ee4aa7be5bf07297381509b2fb2b2e8d33af8e
ry98 <rleahyoung@msn.com>
2017-07-11 12:51:41 -0600

    poller

--- app/src/main/java/clientcommunicator/PollerTask.java
+++ app/src/main/java/clientcommunicator/PollerTask.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,15 +29,14 @@ public class PollerTask extends AsyncTask<Command, Void, CommandResult> {
     private Timer time;
 
     public PollerTask() {
-        this.ip = ip;
-        this.port = port;
+
     }
 
     @Override
     protected CommandResult doInBackground(Command... command) {
 
         time= new Timer();
-        time.schedule(new poll(),0, 1000);
+        time.schedule(new poll(command),0, 1000);
         return new CommandResult();
     }
     public void endTimer()
@@ -51,10 +51,51 @@ public class PollerTask extends AsyncTask<Command, Void, CommandResult> {
     }
     private class poll extends TimerTask
     {
+        private Command[] command;
+        public void poll(Command... command)
+        {
+            this.command=command;
+        }
         @Override
         public void run()
         {
+            try{
+            //set up the URL to get to the command to the correct handler
+            String trimString = "http://" + ip + ":" + port + "/command";
+            URL trimURL = new URL(trimString);
+            HttpURLConnection connection = (HttpURLConnection) trimURL.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
 
+            //convert to JSON
+            Gson gson = new Gson();
+            String jsonIn = gson.toJson(command);
+
+            //send JSON string to server
+            byte[] outputInBytes = jsonIn.getBytes("UTF-8");
+            OutputStream os = connection.getOutputStream();
+            os.write(outputInBytes);
+            os.close();
+
+            connection.connect();
+
+            //get JSON string back from server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder lowerString = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lowerString.append(line);
+            }
+            reader.close();
+            String jsonOut = lowerString.toString();
+
+            //give CommandResult to onPostExecute to handle
+            return gson.fromJson(jsonOut, CommandResult.class);
+
+        }catch (IOException ex){
+        ex.printStackTrace();
+        return null;
+    }
         }
     }
 }
