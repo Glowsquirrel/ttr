51d9a26b4556c5521eb94ee666211a7b060ec80b
ry98 <rleahyoung@msn.com>
2017-07-13 14:58:58 -0600

    start

--- app/src/main/java/model/ClientModel.java
+++ app/src/main/java/model/ClientModel.java
@@ -63,11 +63,12 @@ public class ClientModel implements Observable{
     public void setHasCreatedGame(boolean hasCreatedGame) {
         this.hasCreatedGame = hasCreatedGame;
     }
-
+    
     public boolean isStartedGame() {
+        startedGame=false;
         for(UnstartedGame i:gamesToStart) {
             if(i.getName().equals(myGameName)) {
-                startedGame=i.isStarted();
+                startedGame=true;
             }
         }
         return startedGame;
