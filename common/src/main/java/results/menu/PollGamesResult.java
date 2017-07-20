3797365197cca226efbb4fe285078b4284007596
glowsquirrel <kalan.hunter@byu.net>
2017-07-20 15:24:00 -0600

    Added runningGame class

--- common/src/main/java/results/menu/PollGamesResult.java
+++ common/src/main/java/results/menu/PollGamesResult.java
@@ -2,18 +2,21 @@ package results.menu;
 
 import java.util.List;
 
+import model.RunningGame;
 import results.Result;
 import model.UnstartedGame;
 import utils.Utils;
 
 public class PollGamesResult extends Result {
 
-    protected List<UnstartedGame> gameList;
+    protected List<UnstartedGame> unstartedGameList;
+    protected List<RunningGame> runningGameList;
 
     protected PollGamesResult(){}
-    public PollGamesResult(List<UnstartedGame> gameList){
+    public PollGamesResult(List<UnstartedGame> unstartedGameList, List<RunningGame> runningGameList){
         super.type = Utils.POLL_TYPE;
-        this.gameList = gameList;
+        this.unstartedGameList = unstartedGameList;
+        this.runningGameList = runningGameList;
     }
 
 }
