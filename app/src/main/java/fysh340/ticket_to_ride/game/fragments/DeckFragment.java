2292d84836ff22c909e7ff03ba18a715be20ebeb
sonofafinch <nate@sonofafinch>
2017-07-25 17:56:24 -0600

    Updated the DeckFragment imports.

--- app/src/main/java/fysh340/ticket_to_ride/game/fragments/DeckFragment.java
+++ app/src/main/java/fysh340/ticket_to_ride/game/fragments/DeckFragment.java
@@ -1,6 +1,5 @@
 package fysh340.ticket_to_ride.game.fragments;
 
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -8,9 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 import fysh340.ticket_to_ride.R;
