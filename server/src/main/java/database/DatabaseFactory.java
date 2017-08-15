05b67f61e49c12876b7caf92baa819006dc4259c
Glowsquirrel <kalan.hunter@byu.net>
2017-08-14 20:57:26 -0600

    Plugin for DB.

--- server/src/main/java/database/DatabaseFactory.java
+++ server/src/main/java/database/DatabaseFactory.java
@@ -1,10 +1,18 @@
 package database;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DatabaseFactory {
 
@@ -12,21 +20,44 @@ public class DatabaseFactory {
         IDatabase iDatabase=null;
         String mURLPostfix=null;
         String className=null;
-        switch (databaseType){
-            case "nate":
-                mURLPostfix=File.separator + "server" + File.separator + "src" + File.separator + "main" +
-                        File.separator + "java" + File.separator+ "PersistanceJar" +
-                        File.separator + "nate.jar";
-                className="database.RelationalDatabase.RDBDAO";
-                break;
-            // return new RDBDAO(commandsUntilSave);
-            case "rachael":
-                mURLPostfix=File.separator + "server" + File.separator + "src" + File.separator + "main" +
-                        File.separator + "java" + File.separator+ "PersistanceJar" + File.separator +
-                        "rachael.jar";
-                className="database.FileDatabase.FileDatabase";
 
+        HashMap<String, String> plugins;
+        try {
+            String filePath = "plugins.txt";
+            plugins = new HashMap<String, String>();
+
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine()) != null)
+            {
+                String[] parts = line.split(":", 2);
+                if (parts.length >= 2)
+                {
+                    String key = parts[0];
+                    String value = parts[1];
+                    plugins.put(key, value);
+                } else {
+
+                }
+            }
+
+            for (String key : plugins.keySet())
+            {
+                System.out.println(key + ":" + plugins.get(key));
+            }
+            reader.close();
+        } catch (IOException ex){
+            return null;
         }
+
+
+        if (plugins.get(databaseType) != null){
+            mURLPostfix=File.separator + "server" + File.separator + "src" + File.separator + "main" +
+                    File.separator + "java" + File.separator+ "PersistanceJar" + File.separator;
+            mURLPostfix += databaseType;
+            className = plugins.get(databaseType);
+        }
+
         String pathToDB = Paths.get(".").toAbsolutePath().normalize().toString(); // get working directory
         pathToDB += (mURLPostfix);
         File file  = new File(pathToDB);
