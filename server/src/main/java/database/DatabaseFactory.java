9d80d34f49c6edc0b314ed7fe83fef188fc237c1
shunsambongi <shunsambongi@gmail.com>
2017-08-14 19:57:32 -0600

    Auto stash before merge of "master" and "origin/master"

--- server/src/main/java/database/DatabaseFactory.java
+++ server/src/main/java/database/DatabaseFactory.java
@@ -1,42 +1,46 @@
 package database;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ServiceConfigurationError;
+import java.util.ServiceLoader;
+
 import database.FileDatabase.FileDatabase;
 import database.RelationalDatabase.RDBDAO;
 
 public class DatabaseFactory {
 
     public static IDatabase getDatabase(String databaseType, int commandsUntilSave){
-        IDatabase iDatabase;
-        switch (databaseType){
-            case "nate":
-                return new RDBDAO(commandsUntilSave);
-            case "rachael":
-                FileDatabase fb=new FileDatabase(commandsUntilSave);
-               /* Class<?> c = null;
-                try {
-                    c = Class.forName("plugin.FileDatabase");
-                }
-                catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                try {
-                    iDatabase = (IDatabase)c.newInstance();
-                }
-                catch (InstantiationException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                return iDatabase;*/
-                return fb;
-            default:
-                return new FakeDatabase(commandsUntilSave);
+
+        String jarPath = getJarPath(databaseType, commandsUntilSave);
+
+        File jarFile = new File(jarPath);
+        ClassLoader classLoader;
+        try {
+            classLoader = new URLClassLoader(new URL[] { jarFile.toURI().toURL() });
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
         }
+        ServiceLoader<IDatabase> serviceLoader = ServiceLoader.load(IDatabase.class, classLoader);
+
+        IDatabase database = null;
+
+        try {
+            for (IDatabase factory : serviceLoader) {
+                database = factory;
+            }
+        } catch (ServiceConfigurationError e) {
+            e.printStackTrace();
+            database = null;
+        }
+        return database;
 
     }
 
+    private static String getJarPath(String databaseType, int commandsUntilSave) {
+        return null;
+    }
 }
