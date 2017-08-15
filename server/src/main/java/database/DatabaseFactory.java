12c241535bf7a5124a6681cd50c4ab9f4a499738
ry98 <rleahyoung@msn.com>
2017-08-14 20:24:23 -0600

    change

--- server/src/main/java/database/DatabaseFactory.java
+++ server/src/main/java/database/DatabaseFactory.java
@@ -1,41 +1,71 @@
 package database;
 
-import database.FileDatabase.FileDatabase;
-import database.RelationalDatabase.RDBDAO;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
 
 public class DatabaseFactory {
 
     public static IDatabase getDatabase(String databaseType, int commandsUntilSave){
-        IDatabase iDatabase;
+        IDatabase iDatabase=null;
+        String mURLPostfix=null;
+        String className=null;
         switch (databaseType){
             case "nate":
-                return new RDBDAO(commandsUntilSave);
+                mURLPostfix=File.separator + "server" + File.separator + "src" + File.separator + "main" +
+                        File.separator + "java" + File.separator+ "PersistanceJar" +
+                        File.separator + "nate.jar";
+                className="database.RelationalDatabase.RDBDAO";
+                break;
+               // return new RDBDAO(commandsUntilSave);
             case "rachael":
-                FileDatabase fb=new FileDatabase(commandsUntilSave);
-               /* Class<?> c = null;
-                try {
-                    c = Class.forName("plugin.FileDatabase");
-                }
-                catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                try {
-                    iDatabase = (IDatabase)c.newInstance();
-                }
-                catch (InstantiationException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                return iDatabase;*/
-                return fb;
-            default:
-                return new FakeDatabase(commandsUntilSave);
+                mURLPostfix=File.separator + "server" + File.separator + "src" + File.separator + "main" +
+                        File.separator + "java" + File.separator+ "PersistanceJar" + File.separator +
+                        "rachael.jar";
+                className="database.FileDatabase.FileDatabase";
+
         }
+        String pathToDB = Paths.get(".").toAbsolutePath().normalize().toString(); // get working directory
+        pathToDB += (mURLPostfix);
+        File file  = new File(pathToDB);
+
+        URL url=null;
+        URL[] urls=null;
+        try {
+            url = file.toURL();
+            urls = new URL[]{url};
+        }catch(MalformedURLException ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+
+        ClassLoader cl = new URLClassLoader(urls);
+        // cl.g
+        Class<?> cls=null;
+        try {
+            cls = cl.loadClass(className);
+        }
+        catch(ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+        try {
+             iDatabase = (IDatabase) cls.getDeclaredConstructor(new Class[] {int.class}).newInstance(commandsUntilSave);
+            //iDatabase = (IDatabase) cls.newInstance();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+
+
+
+        return iDatabase;
 
     }
 
